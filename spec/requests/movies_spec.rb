d0088d1a1fd82970d94a664c32c9dcfbdcc7eefa
Tom Billingsley <tomb@antec.local>
2015-09-16 09:11:14 -0400

    verify we prevent empty movie titles

--- spec/requests/movies_spec.rb
+++ spec/requests/movies_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "Movies", type: :request do
 
   before do
       @movie1 = Movie.create :title => "Back to the Future"
+      @taxi_driver_title = 'Taxi Driver'
   end
 
   describe "GET /movies" do
@@ -18,29 +19,42 @@ RSpec.describe "Movies", type: :request do
       click_button 'Create Movie'
       expect(current_path).to eq(movies_path)
       expect(page).to have_content("Citizen Kane")
-      save_and_open_page
     end
   end
 
   describe "PUT /movies" do
     it "edits a movie" do
 
-      taxi_driver_title = 'Taxi Driver'
+
       Movie.destroy_all
-      @movie1 = Movie.create :title => taxi_driver_title
+      @movie1 = Movie.create :title => @taxi_driver_title
 
       visit movies_path
       click_link('Edit')
 
       expect(current_path).to eq(edit_movie_path(@movie1))
 
-      expect(find_field("Title").value ).to eq(taxi_driver_title)
+      expect(find_field("Title").value ).to eq(@taxi_driver_title)
 
       updated_movie_text="Raging Bull"
       fill_in 'Title', :with  => updated_movie_text
       click_button 'Update Movie'
 
       expect(page).to have_content(updated_movie_text)
+    end
+
+    it "should not update an empty task" do
+      Movie.destroy_all
+      @movie1 = Movie.create :title => @taxi_driver_title
+
+      visit movies_path
+      click_link('Edit')
+
+      fill_in 'Title', :with  => ''
+
+      click_button 'Update Movie'
+      expect(current_path).to eq(edit_movie_path(@movie1))
+      expect(page).to have_content("There was an error updating your movie.")
     end
   end
 end
