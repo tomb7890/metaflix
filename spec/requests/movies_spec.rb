4b45ecb00afc79bc341ac3dad0febdd39b907d53
Tom Billingsley <tomb@antec.local>
2015-09-18 07:45:52 -0400

    more tests and some refactoring

--- spec/requests/movies_spec.rb
+++ spec/requests/movies_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 RSpec.describe "Movies", type: :request do
 
   before do
-      @movie1 = Movie.create :title => "Back to the Future"
-      @taxi_driver_title = 'Taxi Driver'
+    @movietitle = "Back to the Future"
+    @movie = Movie.create :title => @movietitle
   end
 
   describe "GET /movies" do
     it "display some movies"  do
       visit movies_path
-      expect(page).to have_content("Back to the Future")
+      expect(page).to have_content(@movietitle)
     end
 
     it "creates a new movie" do
@@ -20,41 +20,44 @@ RSpec.describe "Movies", type: :request do
       expect(current_path).to eq(movies_path)
       expect(page).to have_content("Citizen Kane")
     end
-  end
 
-  describe "PUT /movies" do
-    it "edits a movie" do
+    describe "PUT /movies" do
+      it "edits a movie" do
+        visit movies_path
+        click_link('Edit')
 
+        expect(current_path).to eq(edit_movie_path(@movie))
+        expect( find_field('Title').value).to eq(@movietitle)
 
-      Movie.destroy_all
-      @movie1 = Movie.create :title => @taxi_driver_title
-
-      visit movies_path
-      click_link('Edit')
-
-      expect(current_path).to eq(edit_movie_path(@movie1))
-
-      expect(find_field("Title").value ).to eq(@taxi_driver_title)
-
-      updated_movie_text="Raging Bull"
-      fill_in 'Title', :with  => updated_movie_text
-      click_button 'Update Movie'
-
-      expect(page).to have_content(updated_movie_text)
+        updated_movie_text="Raging Bull"
+        fill_in 'Title', :with  => updated_movie_text
+        click_button 'Update Movie'
+        expect(page).to have_content(updated_movie_text)
+      end
     end
 
     it "should not update an empty task" do
-      Movie.destroy_all
-      @movie1 = Movie.create :title => @taxi_driver_title
-
       visit movies_path
       click_link('Edit')
 
       fill_in 'Title', :with  => ''
-
       click_button 'Update Movie'
-      expect(current_path).to eq(edit_movie_path(@movie1))
+
+      expect(current_path).to eq(edit_movie_path(@movie))
       expect(page).to have_content("There was an error updating your movie.")
+    end
+  end
+
+  describe "DELETE /movies" do
+    it "should delete a movie" do
+      visit movies_path
+
+      expect(current_path).to eq(movies_path)
+
+      within("#movie_#{@movie.id}") do
+        click_link "Delete"
+      end
+      expect(page).to have_content("Movie has been deleted")
     end
   end
 end
