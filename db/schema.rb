b95cfbca3bf4d93b2d4a8b3182df043cd891d3ca
Tom Billingsley <tomb@antec.local>
2015-09-23 08:41:15 -0400

    imdbscore is a decimal not an integer

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150915184528) do
+ActiveRecord::Schema.define(version: 20150923121044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,7 @@ ActiveRecord::Schema.define(version: 20150915184528) do
     t.string   "title"
     t.date     "year"
     t.text     "description"
-    t.integer  "imdbscore"
+    t.decimal  "imdbscore"
     t.integer  "metascore"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
