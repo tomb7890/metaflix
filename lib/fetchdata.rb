be23d78e515040646eb31ca7b365bcdfdb10b4dc
Tom Billingsley <tomb@antec.local>
2015-10-01 06:53:06 -0400

    delete comments and puts statements

--- lib/fetchdata.rb
+++ lib/fetchdata.rb
@@ -16,23 +16,14 @@ class Fetchdata
 
       movieentries = html_doc.xpath("//div[contains(concat(' ', @class, ' '), 'mr_')]")
       movieentries.each do |movieentry|
-        title = movieentry.xpath("div[@class='cover']//a/@title").text() # MOVIE TITLE!!!!
-        description = movieentry.xpath("div[@class='content']//p").first.text() # MOVIE DESCRIPTION
-        year = movieentry.xpath('.//span[@class="year"]').text # MOVIE YEAR
+        title = movieentry.xpath("div[@class='cover']//a/@title").text()
+        description = movieentry.xpath("div[@class='content']//p").first.text()
+        year = movieentry.xpath('.//span[@class="year"]').text
 
         launchurl =   movieentry.xpath("div[@class='content']//a/@href").text()
         imgurl =      movieentry.xpath("div[@class='cover']//img/@src").text()
 
-        puts "[#{title}]"
-        puts "\n"
-        puts "[#{year}]"
-        puts "\n"
-        puts "[#{description}]"
-        puts "\n\n\n"
-
         api = "http://www.omdbapi.com/?t=#{title}&y=#{year}&plot=full&r=json"
-
-        puts ">>>>>>>>>>>>>>>>>>>>>>>> api: #{api}. "
 
         response = HTTParty.get(api)
 
