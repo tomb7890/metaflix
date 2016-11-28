211ed403751d32d90aaa2cf91518db540c07f379
Tom Billingsley <tom.billingsley@gmail.com>
2016-11-28 10:42:22 -0500

    remove obsolete code

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,50 +1,24 @@
 class MoviesController < ApplicationController
   def index
-    @movie = Movie.new
-    @movies = Movie.all
-    @filteredmovies = Movie.metascore_sixty_or_higher.order(:created_at).reverse_order.page  params[:page]
-
+    make_filtered_movies
     make_filtered_movie_dates
   end
 
-  def create
-    Movie.create(user_params)
-    redirect_to :back
-    # render :text => params.inspect
-  end
+  private
 
-  def edit
-    @movie = Movie.find params[:id]
-  end
-
-  def update
-    movie = Movie.find params[:id]
-    if movie.update_attributes user_params # params[:movie]
-      redirect_to movies_path, :notice => "Your movie has successfully been updated."
-    else
-      redirect_to :back, :notice => "There was an error updating your movie."
-    end
-  end
-
-  def destroy
-    Movie.destroy params[:id]
-    redirect_to :back, :notice => 'Movie has been deleted.'
-  end
-
-
-private
-  def user_params
-    params.require(:movie).permit(:title, :description)
+  def make_filtered_movies
+    good_movies = Movie.metascore_sixty_or_higher
+    ordered_good = good_movies.order(:created_at).reverse_order
+    @filteredmovies = ordered_good.page(params[:page])
   end
 
   def make_filtered_movie_dates
-    @filtered_movie_dates = Array.new
-    @filteredmovies.each do | fm |
+    @filtered_movie_dates = []
+    @filteredmovies.each do |fm|
       moviedate = fm.created_at.to_date
-      if not @filtered_movie_dates.include? moviedate
-        @filtered_movie_dates.append( moviedate )
+      unless @filtered_movie_dates.include? moviedate
+        @filtered_movie_dates.append(moviedate)
       end
     end
   end
-
 end
