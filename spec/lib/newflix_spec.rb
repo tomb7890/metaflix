3d2c21ce9d73317fd5e4b2d29f14f50757365ea6
Tom Billingsley <tom.billingsley@gmail.com>
2019-10-23 13:55:41 -0400

    Update tests for recent code changes

--- spec/lib/newflix_spec.rb
+++ spec/lib/newflix_spec.rb
@@ -18,24 +18,24 @@ describe "Newflix" do
   end
 
   it 'must find known good title in today\'s list' do
-    expected = 'The Big Short'
+    expected = 'Only the Brave'
     firstpage = n.page(1)
     list_of_movie_titles = firstpage.map { |m| m['title'] }
     expect(list_of_movie_titles).to include(expected)
     
     mymovie = firstpage.find { |m| m['title'] == expected }
-    expect(mymovie['year']).to eq('2015')
+    expect(mymovie['year']).to eq('2017')
   end
 
   it 'must find known good title in list of another page' do
-    expected = 'American Factory'
+    expected = 'Training Day'
     list_of_movies = n.page(7).map { |m| m['title'] }
     expect(list_of_movies).to include(expected)
   end
   
   it 'must find known good title in list of third page' do
-    expected = 'Screwball'
-    list_of_movies = n.page(10).map { |m| m['title'] }
+    expected = 'El Camino: A Breaking Bad Movie'
+    list_of_movies = n.page(11).map { |m| m['title'] }
     expect(list_of_movies).to include(expected)
   end
 
