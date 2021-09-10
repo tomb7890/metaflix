be3c2afb3cf5af48c3129291fff43ca1df84eef0
Tom Billingsley <tom.billingsley@gmail.com>
2021-09-10 16:40:02 -0400

    DRY up template

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,7 @@
 module MoviesHelper
+
+  def movie_fullprofile_attribute_names
+     %w(Year Rated Released Runtime Genre Director Writer Actors Plot Language Country Awards Metascore imdbRating imdbVotes imdbID Type DVD BoxOffice Production Website )
+  end 
+
 end
